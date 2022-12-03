87e7d0d0aeb32c72204058931e7b85623719c2a0
Kusya Pupkin <95917192+KaRus0n@users.noreply.github.com>
2022-12-03 19:54:35 +0100

    Add files via upload

--- CoreFix/source/SystrayTool.ps1
+++ CoreFix/source/SystrayTool.ps1
@@ -52,92 +52,92 @@ $main_form.AutoSize             = $true
 $main_form.Icon = $FomIcon
 
  #Text, How to use
-$HTU                         = New-Object System.Windows.Forms.Label
-$HTU.Text                     = "How to use"
-$HTU.Location                 = New-Object System.Drawing.Point(23,5)
-$HTU.Font                     = 'Microsoft Sans Serif, 10, style=Bold'
-$HTU.AutoSize                 = $true
+$HTU                            = New-Object System.Windows.Forms.Label
+$HTU.Text                       = "How to use"
+$HTU.Location                   = New-Object System.Drawing.Point(23,5)
+$HTU.Font                       = 'Microsoft Sans Serif, 10, style=Bold'
+$HTU.AutoSize                   = $true
 $main_form.Controls.Add($HTU)
 
  #Text, use1
-$use1                          = New-Object System.Windows.Forms.Label
-$use1.Text                     = '1. To get started, open the file "Config.xml" and customize for yourself.'
-$use1.Location                 = New-Object System.Drawing.Point(15,25)
-$use1.Font                     = 'Microsoft Sans Serif, 8,'
-$use1.AutoSize                 = $true
+$use1                           = New-Object System.Windows.Forms.Label
+$use1.Text                      = '1. To get started, open the file "Config.xml" and customize for yourself.'
+$use1.Location                  = New-Object System.Drawing.Point(15,25)
+$use1.Font                      = 'Microsoft Sans Serif, 8,'
+$use1.AutoSize                  = $true
 $main_form.Controls.Add($use1)
 
  #Text, use2
-$use2                          = New-Object System.Windows.Forms.Label
-$use2.Text                     = '2. Next, restart the application.'
-$use2.Location                 = New-Object System.Drawing.Point(15,45)
-$use2.Font                     = 'Microsoft Sans Serif, 8,'
-$use2.AutoSize                 = $true
+$use2                           = New-Object System.Windows.Forms.Label
+$use2.Text                      = '2. Next, restart the application.'
+$use2.Location                  = New-Object System.Drawing.Point(15,45)
+$use2.Font                      = 'Microsoft Sans Serif, 8,'
+$use2.AutoSize                  = $true
 $main_form.Controls.Add($use2)
 
  #Text, use3
-$use3                          = New-Object System.Windows.Forms.Label
-$use3.Text                     = 'If you did everything right, then there should be no errors.'
-$use3.Location                 = New-Object System.Drawing.Point(20,65)
-$use3.Font                     = 'Microsoft Sans Serif, 8,'
-$use3.AutoSize                 = $true
+$use3                           = New-Object System.Windows.Forms.Label
+$use3.Text                      = 'If you did everything right, then there should be no errors.'
+$use3.Location                  = New-Object System.Drawing.Point(20,65)
+$use3.Font                      = 'Microsoft Sans Serif, 8,'
+$use3.AutoSize                  = $true
 $main_form.Controls.Add($use3)
 
  #Text, use4
-$use4                          = New-Object System.Windows.Forms.Label
-$use4.Text                     = 'Valid values for the "CPU Threads" parameter are in the range'
-$use4.Location                 = New-Object System.Drawing.Point(20,85)
-$use4.Font                     = 'Microsoft Sans Serif, 8,'
-$use4.AutoSize                 = $true
+$use4                           = New-Object System.Windows.Forms.Label
+$use4.Text                      = 'Valid values for the "CPU Threads" parameter are in the range'
+$use4.Location                  = New-Object System.Drawing.Point(20,85)
+$use4.Font                      = 'Microsoft Sans Serif, 8,'
+$use4.AutoSize                  = $true
 $main_form.Controls.Add($use4)
 
  #Text, use5
-$use5                          = New-Object System.Windows.Forms.Label
-$use5.Text                     = 'up to 63 and after 1.844674407371E+19 (64 without conversion)'
-$use5.Location                 = New-Object System.Drawing.Point(20,105)
-$use5.Font                     = 'Microsoft Sans Serif, 8,'
-$use5.AutoSize                 = $true
+$use5                           = New-Object System.Windows.Forms.Label
+$use5.Text                      = 'up to 63 and after 1.844674407371E+19 (64 without conversion)'
+$use5.Location                  = New-Object System.Drawing.Point(20,105)
+$use5.Font                      = 'Microsoft Sans Serif, 8,'
+$use5.AutoSize                  = $true
 $main_form.Controls.Add($use5)
 
  #Text, use6
-$use6                          = New-Object System.Windows.Forms.Label
-$use6.Text                     = "Please email me if you have any bugs that you can't resolve."
-$use6.Location                 = New-Object System.Drawing.Point(15,130)
-$use6.Font                     = 'Microsoft Sans Serif, 9'
-$use6.AutoSize                 = $true
+$use6                           = New-Object System.Windows.Forms.Label
+$use6.Text                      = "Please email me if you have any bugs that you can't resolve."
+$use6.Location                  = New-Object System.Drawing.Point(15,130)
+$use6.Font                      = 'Microsoft Sans Serif, 9'
+$use6.AutoSize                  = $true
 $main_form.Controls.Add($use6)
 
 
  #Text, Contacts
-$Contacts                          = New-Object System.Windows.Forms.Label
-$Contacts.Text                     = "Contacts"
-$Contacts.Location                 = New-Object System.Drawing.Point(400,5)
-$Contacts.Font                     = 'Microsoft Sans Serif, 10, style=Bold'
-$Contacts.AutoSize                 = $true
+$Contacts                       = New-Object System.Windows.Forms.Label
+$Contacts.Text                  = "Contacts"
+$Contacts.Location              = New-Object System.Drawing.Point(400,5)
+$Contacts.Font                  = 'Microsoft Sans Serif, 10, style=Bold'
+$Contacts.AutoSize              = $true
 $main_form.Controls.Add($Contacts)
 
  #Button "Discord"
 $Button_Discord                 = New-Object System.Windows.Forms.Button
-$Button_Discord.Location         = New-Object System.Drawing.Size(390,25)
-$Button_Discord.Size             = New-Object System.Drawing.Size(85,23)
+$Button_Discord.Location        = New-Object System.Drawing.Size(390,25)
+$Button_Discord.Size            = New-Object System.Drawing.Size(85,23)
 $Button_Discord.Add_Click({Start-Process "https://discordapp.com/users/412980274935103508/"})
-$Button_Discord.Text             = "Discord"
+$Button_Discord.Text            = "Discord"
 $main_form.Controls.Add($Button_Discord)
 
  #Button "Telegram"
-$Button_Telegram                  = New-Object System.Windows.Forms.Button
-$Button_Telegram.Location         = New-Object System.Drawing.Size(390,50)
-$Button_Telegram.Size             = New-Object System.Drawing.Size(85,23)
+$Button_Telegram                = New-Object System.Windows.Forms.Button
+$Button_Telegram.Location       = New-Object System.Drawing.Size(390,50)
+$Button_Telegram.Size           = New-Object System.Drawing.Size(85,23)
 $Button_Telegram.Add_Click({Start-Process "https://t.me/C0RE2quad"})
-$Button_Telegram.Text             = "Telegram"
+$Button_Telegram.Text           = "Telegram"
 $main_form.Controls.Add($Button_Telegram)
 
  #Button "VK"
-$Button_VK                  = New-Object System.Windows.Forms.Button
-$Button_Vk.Location         = New-Object System.Drawing.Size(390,75)
-$Button_VK.Size             = New-Object System.Drawing.Size(85,23)
+$Button_VK                      = New-Object System.Windows.Forms.Button
+$Button_Vk.Location             = New-Object System.Drawing.Size(390,75)
+$Button_VK.Size                 = New-Object System.Drawing.Size(85,23)
 $Button_VK.Add_Click({Start-Process "https://vk.com/karus0n"})
-$Button_VK.Text             = "VK"
+$Button_VK.Text                 = "VK"
 $main_form.Controls.Add($Button_VK)
 
  #Button "cancel"
